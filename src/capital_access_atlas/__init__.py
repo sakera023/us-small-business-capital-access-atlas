7f48c588f551626460f29603e63a1ea0d837bf94
Sakera Begum <sakerasiu23@gmail.com>
2026-09-03 04:23:39 -0400

    Expand public research API

--- src/capital_access_atlas/__init__.py
+++ src/capital_access_atlas/__init__.py
@@ -1,9 +1,16 @@
 """Public research utilities for the U.S. Small Business Capital Access Atlas."""
 
+from .analysis import leave_one_metric_out_sensitivity, metric_quality_report
+from .census_cbp import (
+    CENSUS_CBP_2023,
+    load_cbp_state_file,
+    summarize_cbp_state_totals,
+)
 from .geography import (
     US_STATE_NAMES,
     detect_state_column,
     normalize_state_abbreviation,
+    numeric_metric_columns,
     prepare_state_metric,
 )
 from .indicators import build_composite_index
@@ -14,12 +21,18 @@ from .public_data import (
 )
 
 __all__ = [
+    "CENSUS_CBP_2023",
     "SBA_STATE_DATASET",
     "US_STATE_NAMES",
     "build_composite_index",
     "detect_state_column",
     "get_sba_state_metadata",
+    "leave_one_metric_out_sensitivity",
+    "load_cbp_state_file",
     "load_sba_state_workbook",
+    "metric_quality_report",
     "normalize_state_abbreviation",
+    "numeric_metric_columns",
     "prepare_state_metric",
+    "summarize_cbp_state_totals",
 ]
